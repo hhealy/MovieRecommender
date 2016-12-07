a1c0d30db76742b5eae051220ce4de56da30581d
Henry Healy <henryjdhealy@gmail.com>
2016-12-07 01:27:55 +0000

    Added the moviedata_small and added to the interface and the API.

--- src/controllers/MovieRecommenderAPI.java
+++ src/controllers/MovieRecommenderAPI.java
@@ -1,6 +1,73 @@
 package controllers;
 
-public class MovieRecommenderAPI 
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Movie;
+import models.Rating;
+import models.Recommendation;
+
+public class MovieRecommenderAPI implements MovieRecommenderInterface
 {
+
+	@Override
+	public void addUser(String firstName, String lastName, int age, char gender, String occupation) 
+	{
+		
+	}
+
+	@Override
+	public void removeUser(double userID) 
+	{
+		
+	}
+
+	@Override
+	public void addMovie(String title, int year, String url) 
+	{
+		
+	}
+
+	@Override
+	public void addRating(double userID, double movieID, int rating) 
+	{
+		
+	}
+
+	@Override
+	public String getMovie(double movieID) 
+	{
+		return null;
+	}
+
+	@Override
+	public int getUserRating(double userID) 
+	{
+		return 0;
+	}
+
+	@Override
+	public String getUserRecommendation(double userID) 
+	{
+		return null;
+	}
+
+	@Override
+	public String getTopTenMovies() 
+	{
+		return null;
+	}
+
+	@Override
+	public void load() 
+	{
+		
+	}
+
+	@Override
+	public void write() 
+	{
+		
+	}
 
 }
---
+++ src/controllers/MovieRecommenderInterface.java
@@ -0,0 +1,34 @@
+package controllers;
+
+public interface MovieRecommenderInterface 
+{
+	//Add a user to the program.
+	public void addUser(String firstName, String lastName, int age, char gender, String occupation);
+	
+	//Remove a user from the program.
+	public void removeUser(double userID);
+	
+	//Add a movie to the program.
+	public void addMovie(String title, int year, String url);
+	
+	//Add a movie rating to the program.
+	public void addRating(double userID, double movieID, int rating);
+	
+	//Returns the name of a movie.
+	public String getMovie(double movieID);
+	
+	//Returns the user rating of a movie.
+	public int getUserRating(double userID);
+	
+	//Returns the user recommendations.
+	public String getUserRecommendation(double userID);
+	
+	//Returns the top ten movies.
+	public String getTopTenMovies();
+	
+	//Load from the serializer
+	public void load();
+	
+	//Write to the serializer
+	public void write();
+}
